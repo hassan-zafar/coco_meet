58a697da5e20d4a9080d259dbd77dd8ca6ac4070
hassan-zafar <hz.asd1@gmail.com>
2022-10-26 01:30:36 +0500

    added empty notification screen

---
+++ lib/screens/HomePage Screens/notification_screen.dart
@@ -0,0 +1,24 @@
+import 'package:coco_meet/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset(emptyNotificationImg),
+          const Text('Empty'),
+          const Text("You don't have any notifications at this time"),
+        ],
+      ),
+    );
+  }
+}
